de79445841b151d77f5fe72d0b854536e1028011
Junjie Wu <junjiew@codeaurora.org>
2015-11-20 16:10:58 -0800

    qcom-cpufreq: Use cpufreq_table_validate_and_show to fill freq_table
    cpufreq_table_validate_and_show() is created as a standard way to
    initialize frequency for a policy and assign freq_table. Use this new
    API to assign freq_table.
    
    CRs-Fixed: 1024229
    Change-Id: Iac3a9394790e140492c5c0c0ad6d068840bfffa4
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -153,8 +153,8 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 		if (cpu_clk[cpu] == cpu_clk[policy->cpu])
 			cpumask_set_cpu(cpu, policy->cpus);
 
-	if (cpufreq_frequency_table_cpuinfo(policy, table))
-	{
+	ret = cpufreq_table_validate_and_show(policy, table);
+	if (ret) {
 		// AP: set default frequencies to prevent overclocking or underclocking during start
 		if (policy->cpu <= 3)
 		{
@@ -169,6 +169,7 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 		}
 
 		pr_err("cpufreq: failed to get policy min/max\n");
+		return ret;
 	}
 
 	// AP: set default frequencies to prevent overclocking or underclocking during start
@@ -205,7 +206,6 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
 			policy->cpu, cur_freq, table[index].frequency);
 	policy->cur = table[index].frequency;
-	policy->freq_table = table;
 #ifdef CONFIG_MSM_TRACK_FREQ_TARGET_INDEX
 	policy->cur_index = index;
 #endif
