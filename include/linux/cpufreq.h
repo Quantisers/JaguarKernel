369332a8c2d11e0524ea63f6eaa2042883e29970
Chris Redpath <chris.redpath@arm.com>
2017-03-01 13:42:58 +0000

    cpufreq/schedutil: Fix schedutil's 'default governor' machinery
    schedutil was not integrated with the default governor machinery, so
    you were unable to select it. Update the table and rename the governor
    object for consistency with the rest of the cpufreq governors.
    
    Change-Id: I9ab5046ad5ba675d7dbed7c3e2046e06f8013c72
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -587,6 +587,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
+extern struct cpufreq_governor cpufreq_gov_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
