6511cbddf5500daa8f519be49e94b4ab5758ef44
Haseo97 <jawgattus@gmail.com>
2018-04-17 16:48:15 +0700

    GPU: Trying to add more regulator
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -766,6 +766,8 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8937_475MHz[] = {
 	F_SLEW( 400000000, FIXED_CLK_SRC, gpll0,	2,	0,	0),
 	F_SLEW( 450000000, 900000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 475000000, 950000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 525000000, 1050000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 600000000, 1100000000,	  gpll3,	1,	0,	0),
 	F_END
 };
 
@@ -786,7 +788,8 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8940_500MHz[] = {
 	F_SLEW( 400000000, FIXED_CLK_SRC, gpll0,	2,	0,	0),
 	F_SLEW( 450000000, 900000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 475000000, 950000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 500000000, 1000000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 525000000, 1000000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 600000000, 1050000000,	  gpll3,	1,	0,	0),
 	F_END
 };
 
@@ -4238,7 +4241,7 @@ static void override_for_8937(int speed_bin)
 		OVERRIDE_FMAX6(gfx3d,
 			LOWER, 216000000, LOW, 300000000,
 			NOMINAL, 375000000, NOM_PLUS, 400000000,
-			HIGH, 450000000, SUPER_TUR, 475000000);
+			HIGH, 450000000, SUPER_TUR, 600000000);
 		OVERRIDE_FTABLE(gfx3d, ftbl_gcc_oxili_gfx3d_clk, 8937_475MHz);
 	} else {
 		OVERRIDE_FMAX5(gfx3d,
