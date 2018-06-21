be7ea7d1cec124e27dcf640905bb17e7126a5266
Team Quanta <lmkk2k18@gmail.com>
2018-06-21 08:36:31 +0530

    remove out folder

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Mon Jun 18 11:12:43 IST 2018"
-#define LINUX_COMPILE_BY "QuantumMechanic"
-#define LINUX_COMPILE_HOST "TEAMQUANTUM"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC)"
