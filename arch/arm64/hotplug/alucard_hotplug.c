d246ce55fa7c3fecaec9dd0439452f72348359cb
dencel007 <dencelkbabu007@gmail.com>
2018-06-24 08:27:43 +0530

    :sparkles: hotplug: set Alucard as default hotplug
    Signed-off-by: Dencel K Babu <dencelkbabu007@gmail.com>
    Signed-off-by: infinitEplus <navdhareharsh2001@gmail.com>

--- arch/arm64/hotplug/alucard_hotplug.c
+++ arch/arm64/hotplug/alucard_hotplug.c
@@ -68,7 +68,7 @@ static struct hotplug_tuners {
 #endif
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 65,
-	.hotplug_enable = 0,
+	.hotplug_enable = 1,
 	.min_cpus_online = 1,
 	.maxcoreslimit = 8,
 	.maxcoreslimit_sleep = 1,
