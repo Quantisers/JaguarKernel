4c9198bccc4ca554d0200910b5a9b205c686c675
bitrvmpd <e.noyer.silva@gmail.com>
2018-09-10 16:35:26 +0000

    :bug: Fixes msm_vdec compilation

--- drivers/media/platform/msm/vidc/msm_vdec.c
+++ drivers/media/platform/msm/vidc/msm_vdec.c
@@ -1512,11 +1512,11 @@ static int msm_vdec_queue_setup(struct vb2_queue *q,
 		 * than 4 subframes requires more than 4
 		 * reference frames to decode.
 		 */
-		if (inst->fmts[OUTPUT_PORT].fourcc ==
+		if (inst->fmts[OUTPUT_PORT]->fourcc ==
 				V4L2_PIX_FMT_VP9 &&
 				*num_buffers < MIN_NUM_OUTPUT_BUFFERS_VP9)
 			*num_buffers = MIN_NUM_OUTPUT_BUFFERS_VP9;
-		else if (inst->fmts[OUTPUT_PORT].fourcc ==
+		else if (inst->fmts[OUTPUT_PORT]->fourcc ==
 				V4L2_PIX_FMT_HEVC &&
 				*num_buffers < MIN_NUM_OUTPUT_BUFFERS_HEVC)
 			*num_buffers = MIN_NUM_OUTPUT_BUFFERS_HEVC;
