6f01b6a92426f61ebed2db1f42eb654c84ff1d33
Quantummech2000 <vvr-rockstar@bmsit.in>
2018-10-25 15:58:17 +0000

    :hammer: cpufreq: Add Impulse governor
    Signed-off-by: Quantummech2000 <vvr-rockstar@bmsit.in>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -549,6 +549,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
 extern struct cpufreq_governor cpufreq_gov_lionfish;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionfish)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
 extern struct cpufreq_governor cpufreq_gov_nebula;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
